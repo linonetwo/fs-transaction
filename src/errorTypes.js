c7e1084fb6b98c8df998115a6728461652900858
lin onetwo <lindongwu11@gmail.com>
2016-09-25 14:29:04 +0800

    add more errortypes

--- src/errorTypes.js
+++ src/errorTypes.js
@@ -1,2 +1,2 @@
-export const MISSING_IMPORTANT_FILE = `missing-important-file transaction-fs need things like tempfolder and basepath to work, if they prooved to be not exist, fs-transaction won\'t work properly`;
-export const 
+export const MISSING_IMPORTANT_FILE = 'missing-important-file transaction-fs need things like tempfolder and basepath to work, if they prooved to be not exist, fs-transaction won\'t work properly  ';
+export const ALREADY_EXIST = 'already-exists, this may means you use an uuid or something for filename that has already been used  ';
